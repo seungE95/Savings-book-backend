fb95144660387e525d67c2e984dddde00e1bbb99
seungE95 <ewotmd95@naver.com>
2023-08-23 23:19:42 +0900

    cors

--- src/init.js
+++ src/init.js
@@ -2,15 +2,22 @@ import express from "express";
 import userRouter from "../routers/userRouter.js";
 import morgan from "morgan";
 import db from "../db.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 const logger = morgan("dev");
 
+app.use(cors(corsConfig));
 app.use(express.urlencoded({extended: true}));
 app.use(logger);
 app.use("/", userRouter);
 app.use("/api/user", userRouter);
+
+const corsConfig = {
+    origin: "https://port-0-savings-book-backend-eu1k2llladze0x.sel3.cloudtype.app/",
+    credentials: true,
+};
 
 const handleListener = () => {
     console.log(`Hello! Savings Book start http://localhost:${PORT}`);
