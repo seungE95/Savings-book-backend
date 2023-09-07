c904a68dd97c6a37954fc1ff75ae513561e76752
seungE95 <ewotmd95@naver.com>
2023-09-07 17:44:31 +0900

    json

--- src/init.js
+++ src/init.js
@@ -9,6 +9,7 @@ const app = express();
 const PORT = 3000;
 const logger = morgan("dev");
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(logger);
