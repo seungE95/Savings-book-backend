c6bf28ce209763c6404351561670f2362b7fd2aa
seungE95 <ewotmd95@naver.com>
2023-09-07 15:51:36 +0900

    cors

--- src/init.js
+++ src/init.js
@@ -3,13 +3,13 @@ import userRouter from "../routers/userRouter.js";
 import mainRouter from "../routers/mainRouter.js"
 import morgan from "morgan";
 import db from "../db.js";
-//import cors from "cors";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 const logger = morgan("dev");
 
-//app.use(cors());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
 app.use("/", userRouter);
