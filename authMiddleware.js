6b7783a6325dbd3450bab7a5e1f04f4334855cbc
seungE95 <ewotmd95@naver.com>
2023-09-11 11:17:55 +0900

    token 수정

--- authMiddleware.js
+++ authMiddleware.js
@@ -7,8 +7,10 @@ const key = process.env.SECRET_KEY;
 export const auth = (req, res, next) => {
     //인증 완료
     try {
+        console.log("\naccess_token::: "+ req.headers.access_token.trim());
+        //console.log("\nauthorization::: "+ req.headers.authorization.split('Bearer ')[1]);
         //요청 헤더에 저장된 토큰(req.headers.authorization)과 비밀키를 사용하여 토큰을 req.decoded에 반환
-        req.decoded = jwt.verify(req.headers.access_token, key);//.split('Bearer ')[1], key);
+        req.decoded = jwt.verify(req.headers.access_token.trim(), key);//.split('Bearer ')[1], key);
         return next();
     } catch (error) {
         //인증 실패
