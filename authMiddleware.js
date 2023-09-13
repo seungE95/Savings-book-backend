84644e8d5e7f0618e19065cced1e0fa93ba1a50b
seungE95 <ewotmd95@naver.com>
2023-09-13 10:31:48 +0900

    수정

--- authMiddleware.js
+++ authMiddleware.js
@@ -8,11 +8,13 @@ export const auth = (req, res, next) => {
     //인증 완료
     try {
         console.log("\nHeaders::: "+ JSON.stringify(req.headers));
-        console.log("\nheader ::: "+ req.headers["access_token"]);
-        console.log("\naccess_token::: "+ req.headers.access_token);
+        console.log("\nbody::: "+ JSON.stringify(req.body));
+        console.log("\nreq.headers.access_token::: "+ req.headers.access_token);
+        console.log("\nreq.headers.authorization::: "+ req.headers.authorization);
+
         //console.log("\nauthorization::: "+ req.headers.authorization.split('Bearer ')[1]);
         //요청 헤더에 저장된 토큰(req.headers.authorization)과 비밀키를 사용하여 토큰을 req.decoded에 반환
-        req.decoded = jwt.verify(req.headers.authorization, key);//.split('Bearer ')[1], key);
+        req.decoded = jwt.verify(req.headers.access_token, key);//.split('Bearer ')[1], key);
         return next();
     } catch (error) {
         //인증 실패
